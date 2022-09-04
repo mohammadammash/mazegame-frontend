d13d04f355af3ad8c9455420b8fde2e9a3cd09c6
mohammadammash <mohammadammash31@gmail.com>
2022-09-04 19:02:46 +0300

    Adding LocalStorage to store users scores with their unique username in 'js/app.js'

--- js/app.js
+++ js/app.js
@@ -11,29 +11,34 @@ var lose = false;
 var score = 0;
 var orgStatus = sts.textContent;
 
+// Intialzing User in Storage and loading storage data
+var username = prompt("Please enter your username: ");
+if (!username) username = prompt("Please enter your username: ");
+var usernames = loadLocalStorage();
+
 // Main Game:
 start.addEventListener("mouseover", () => {
   reset();
   win = false;
   lose = false;
-
   catchCheating();
   touchBoundaries();
 
+  // if the user reached the end(win): update Score, headerText, and database
   end.addEventListener("mouseover", () => {
     if (!lose && !win) {
       sts.textContent = "You Won!!";
       win = true;
-      winNow = true;
       score += 5;
       scoreShow.textContent = score;
+      updateLocalStorage();
     }
   });
 });
 
-// -----FUNCTIONS-------:
 
-// reset walls to original color
+// -----FUNCTIONS-------:
+// reset walls to original color and reset headerContent
 function reset() {
   for (bd of boundaries) {
     bd.classList.remove("youlose");
@@ -41,14 +46,14 @@ function reset() {
   sts.textContent = orgStatus;
 }
 
-// Turn walls color into red:
+// Turn walls color into red(lost)
 function redWallsColor() {
   for (bd of boundaries) {
     bd.classList.add("youlose");
   }
 }
 
-// user touches boundaries:
+// when user touches boundaries(lost): update boundariesColors, headerText, score, and database
 function touchBoundaries() {
   for (bd of boundaries) {
     bd.addEventListener("mouseover", () => {
@@ -58,12 +63,13 @@ function touchBoundaries() {
         lose = true;
         score -= 10;
         scoreShow.textContent = score;
+        updateLocalStorage();
       }
     });
   }
 }
 
-// user catched cheating (got out of gameBoard)
+// user catched cheating(got out of gameBoard): update boundariesColors, and headerText.
 function catchCheating() {
   gameBoard.addEventListener("mouseleave", () => {
     if (!win && !lose) {
@@ -72,4 +78,29 @@ function catchCheating() {
       lose = true;
     }
   });
+}
+
+
+// DB - LOCALSTORAGE
+async function loadLocalStorage() {
+  var usernames = localStorage.getItem("usernames");
+  if (!usernames) {
+    var usernames = {};
+  } else {
+    usernames = JSON.parse(usernames);
+  }
+  if (username in usernames) {
+    scoreShow.textContent = usernames[username];
+    score = parseInt(usernames[username]);
+  } else {
+    usernames[username] = "0";
+    localStorage.setItem("usernames", JSON.stringify(usernames));
+  }
+
+  return usernames;
+}
+
+function updateLocalStorage() {
+  usernames[username] = JSON.stringify(score);
+  localStorage.setItem("usernames", JSON.stringify(usernames));
 }
